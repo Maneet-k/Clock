4f9648d11149dc0664a8a4b9349f4569f590bdb7
Maneet-k <81208462+Maneet-k@users.noreply.github.com>
2021-07-06 18:07:16 +0530

    qoom sync at Tue Jul 06 2021 12:37:16 GMT+0000 (Coordinated Universal Time)

---
+++ script.js
@@ -0,0 +1,9 @@
+function dark() {
+	document.body.style.backgroundColor = "#262626";
+document.body.style.color = "#f2f2f2";
+}
+
+function light() {
+	document.body.style.backgroundColor = "#99bbff";
+	document.body.style.color = "black";
+}
